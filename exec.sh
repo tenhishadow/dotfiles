afffb7fa16546d2e29650d4e06a01d485a9172da
tenhishadow <adm@tenhi.dev>
2020-02-07 02:01:39 +0300

    exec: update script

--- exec.sh
+++ exec.sh
@@ -1,5 +1,14 @@
-virtualenv ansible_venv
-source ansible_venv/bin/activate
+#!/bin/bash
+
+ANSIBLE_VENV_DIR="./ansible_venv"
+
+# check for venv || create it
+[[ ! -r "${ANSIBLE_VENV_DIR}/bin/activate" ]] && virtualenv ${ANSIBLE_VENV_DIR}
+
+# shellcheck disable=SC1090
+source ${ANSIBLE_VENV_DIR}/bin/activate
+
 pip install -r requirements.txt
 
 ansible-playbook playbook_install.yml
+
