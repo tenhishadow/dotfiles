ee7c3694831c95ea0cbe75f60c911cf7154e92e9
tenhishadow <adm@tenhi.dev>
2020-02-07 03:43:48 +0300

    exec: add mitogen :)

--- exec.sh
+++ exec.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# vars
 ANSIBLE_VENV_DIR="./ansible_venv"
+
+# functions
+function fn_mitogen {
+  ANSIBLE_STRATEGY_PLUGINS=$( find  "${ANSIBLE_VENV_DIR}" -type d -name strategy | grep mitogen )
+  if [[ -d $ANSIBLE_STRATEGY_PLUGINS ]]
+  then
+    export ANSIBLE_STRATEGY_PLUGINS
+    export ANSIBLE_STRATEGY="mitogen_linear"
+  fi
+}
 
 # check for venv || create it
 [[ ! -r "${ANSIBLE_VENV_DIR}/bin/activate" ]] && virtualenv ${ANSIBLE_VENV_DIR}
@@ -9,6 +20,8 @@ ANSIBLE_VENV_DIR="./ansible_venv"
 source ${ANSIBLE_VENV_DIR}/bin/activate
 
 pip install -r requirements.txt
+
+fn_mitogen
 
 ansible-playbook playbook_install.yml
 
